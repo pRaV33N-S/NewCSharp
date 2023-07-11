0855856c49ba2de2dfaced7b393f0beb5c32216b
pRaV33N-S <praveen.srinivasan1@mphasis.com>
2023-07-11 09:41:16 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Welcome to C# Tutorials");
+            //Console.ReadKey();
+            string firstName;
+            string lastName;
+            Console.WriteLine("Enter First Name");
+            firstName=Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            lastName = Console.ReadLine();
+            Console.WriteLine("Welcome Mr.\\Ms." + firstName + "" + lastName);
+            Console.ReadKey();
+        }
+    }
+}
